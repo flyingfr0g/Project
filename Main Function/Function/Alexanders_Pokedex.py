600f20d15f2a50c6b9101ccf9bf773c760cb4496
flyingfr0g <alexander.b.scull@gmail.com>
2020-12-22 19:28:26 -0500

    Almost there

--- Main Function/Function/Alexanders_Pokedex.py
+++ Main Function/Function/Alexanders_Pokedex.py
@@ -25,9 +25,60 @@ def pokedex():
     type_info = poke.get('types')
     abilities_info = poke.get('abilities')
     poke_name = (name_info['name'])
-    print((poke_name), ": which is classified as a: "),
+    print((poke_name), ", which is classified as a: "),
     for t in type_info:
         print((t['type']['name']), "", sep=" type")
+        if t['type']['name'] == "grass":
+            print("grass types are weak against fire, bug, flying, ice, and poison types")
+
+        if t['type']['name'] == "poison":
+            print("poison types are weak against ground, and psychic types")
+
+        if t['type']['name'] == "normal":
+            print("normal types are weak against fighting, and ghost types")
+
+        if t['type']['name'] == "bug":
+            print("bug types are weak against fire, flying, and rock types")
+
+        if t['type']['name'] == "electric":
+            print("electric types are weak against ground types")
+
+        if t['type']['name'] == "rock":
+            print("rock types are weak against fighting, ground, steel, water and grass types")
+
+        if t['type']['name'] == "dark":
+            print("dark types are weak against bug, fighting, and fairy types")
+
+        if t['type']['name'] == "fairy":
+            print("fairy types are weak against poison, and steel types")
+
+        if t['type']['name'] == "flying":
+            print("bug types are weak against electric, ice, and water types")
+
+        if t['type']['name'] == "ground":
+            print("ground types are weak against grass, ice, and water types")
+
+        if t['type']['name'] == "steel":
+            print("steel types are weak against fire, fighting, and ground types")
+
+        if t['type']['name'] == "dragon":
+            print("dragon types are weak against dragon, ice, and fairy types")
+
+        if t['type']['name'] == "fighting":
+            print("fighting types are weak against fairy, flying, and psychic types")
+
+        if t['type']['name'] == "ghost":
+            print("ghost types are weak against ghost, and dark types")
+
+        if t['type']['name'] == "ice":
+            print("ice types are weak against fire, fighting, steel, and rock types")
+
+        if t['type']['name'] == "water":
+            print("water types are weak against electric, and grass types")
+
+        if t['type']['name'] == "psychic":
+            print("psychic types are weak against bug, dark, and ghost types")
+
     print( "this Pokemon's signature abilities are: ")
     for a in abilities_info:
             print((a['ability']['name']),"", sep=",")
